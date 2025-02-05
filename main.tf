9d61f5b74e63c5ab2dbde5e45ce62e27dc820266
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-02-05 16:14:23 +0530

    hello

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "us-east-1"  # Replace with your desired region
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.0.0/24"
