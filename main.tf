4e7f14105e4b5e55f4f525df57962d69c16bafb6
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-06-16 14:16:05 +0530

    Athena tagging

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_region" "current" {}
-
 # Replace this with your AWS Account ID
 variable "account_id" {
   default = "122610525295"
@@ -31,11 +29,8 @@ resource "aws_athena_workgroup" "example" {
     publish_cloudwatch_metrics_enabled = true
 
     result_configuration {
-      output_location = "s3://${aws_s3_bucket.example.bucket}/output/"
-
       encryption_configuration {
         encryption_option = "SSE_KMS"
-        kms_key_arn       = aws_kms_key.example.arn
       }
     }
   }
