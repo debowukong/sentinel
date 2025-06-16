4a56d84e57aab77b4c0bd299f1236d7d462badce
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-06-16 18:05:38 +0530

    AWS Cloudtrail

--- main.tf
+++ main.tf
@@ -3,11 +3,15 @@ provider "aws" {
 }
 
 resource "aws_cloudtrail" "example" {
-  # ... other configuration ...
+  name                          = "example-cloudtrail"
+  s3_bucket_name                = "your-cloudtrail-bucket-name"
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_logging                = true
 
   event_selector {
     read_write_type           = "All"
     include_management_events = true
-
   }
 }
+
