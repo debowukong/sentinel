5278a6bb7df2b711945d27826f9b0d8422328c32
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-06-16 17:55:59 +0530

    Hello

--- main.tf
+++ main.tf
@@ -9,9 +9,5 @@ resource "aws_cloudtrail" "example" {
     read_write_type           = "All"
     include_management_events = true
 
-    data_resource {
-      type   = "AWS::S3::Object"
-      values = ["arn:aws:s3"]
-    }
   }
 }
