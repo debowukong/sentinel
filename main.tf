293fbf7a3dad55d4e004c2dfdca0930f6d753fd4
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-06-17 11:13:57 +0530

    hello

--- main.tf
+++ main.tf
@@ -2,18 +2,98 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_cloudtrail" "example" {
-  name                          = "example-cloudtrail"
-  s3_bucket_name                = "your-cloudtrail-bucket-name"
-  include_global_service_events = true
-  is_multi_region_trail         = true
-  enable_logging                = true
+provider "aws" {
+  region = "us-east-1"
+}
 
-  event_selector {
-    read_write_type                 = "All"
-    include_management_events       = true
-    exclude_management_event_sources = [
-      "events.amazonaws.com"
+##############################
+# 1. S3 Bucket for Results
+##############################
+
+resource "aws_s3_bucket" "athena_results" {
+  bucket = "athena-query-results-123456"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket                  = aws_s3_bucket.athena_results.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+##############################
+# 2. IAM Policy
+##############################
+
+data "aws_iam_policy_document" "athena_least_privilege" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject"
+    ]
+    resources = [
+      aws_s3_bucket.athena_results.arn,
+      "${aws_s3_bucket.athena_results.arn}/*"
     ]
   }
+
+  statement {
+    actions = [
+      "athena:StartQueryExecution",
+      "athena:GetQueryExecution",
+      "athena:GetQueryResults"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "athena_policy" {
+  name        = "AthenaLeastPrivilegePolicy"
+  policy      = data.aws_iam_policy_document.athena_least_privilege.json
+}
+
+##############################
+# 3. IAM Role for Execution
+##############################
+
+data "aws_iam_policy_document" "assume_athena" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["athena.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "athena_execution_role" {
+  name               = "AthenaExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_athena.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.athena_execution_role.name
+  policy_arn = aws_iam_policy.athena_policy.arn
+}
+
+##############################
+# 4. Athena Workgroup
+##############################
+
+resource "aws_athena_workgroup" "secure_workgroup" {
+  name = "secure-athena-workgroup"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.athena_results.bucket}/"
+    }
+
+    execution_role = aws_iam_role.athena_execution_role.arn
+  }
+
+  state = "ENABLED"
 }
