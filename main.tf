1077c8eb3db34254e50c0bda637ea01db5251031
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-06-24 13:46:09 +0530

    Updated memacached cluster

--- main.tf
+++ main.tf
@@ -8,5 +8,6 @@ resource "aws_elasticache_cluster" "example" {
   node_type            = "cache.m4.large"
   num_cache_nodes      = 2
   parameter_group_name = "default.memcached1.4"
+  apply_immediately    = true
   port                 = 11211
 }
