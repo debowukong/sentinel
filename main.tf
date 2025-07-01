4d46cc19366cd32a18b9b28d2519681e2f935a8a
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-01 10:27:37 +0530

    secrets maanger

--- main.tf
+++ main.tf
@@ -2,15 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_elasticache_cluster" "example" {
-  cluster_id           = "cluster-example"
-  engine               = "memcached"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 2
-  parameter_group_name = "default.memcached1.4"
-  apply_immediately    = true
-  port                 = 11211
-  az_mode              = cross-az
+resource "aws_secretsmanager_secret" "example" {
+  name = "example"
 
-  maintenance_window   = "sun:05:00-sun:06:00" # Approved window
+  tags = {
+    Environment = "dev"
+    Owner       = "team-security"
+    Purpose     = "store-credentials"
+  }
 }
