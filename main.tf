58d42353347f6df5e85d24a2755e86261c1b7c9a
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-02 10:39:05 +0530

    iam policy doucment

--- main.tf
+++ main.tf
@@ -4,10 +4,24 @@ provider "aws" {
 
 resource "aws_secretsmanager_secret" "example" {
   name = "example"
+}
 
-  tags = {
-    Environment = "dev"
-    Owner       = "team-security"
-    Purpose     = "store-credentials"
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid    = "EnableAnotherAWSAccountToReadTheSecret"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::123456789012:root"]
+    }
+
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["*"]
   }
+}
+
+resource "aws_secretsmanager_secret_policy" "example" {
+  secret_arn = aws_secretsmanager_secret.example.arn
+  policy     = data.aws_iam_policy_document.example.json
 }
