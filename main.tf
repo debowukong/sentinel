dda7b320189a331c02d02ea6afc64d0a255c0cc1
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-02 11:09:51 +0530

    Hello world

--- main.tf
+++ main.tf
@@ -2,26 +2,54 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# 1. Create the secret
 resource "aws_secretsmanager_secret" "example" {
   name = "example"
 }
 
+# 2. IAM Policy Document allowing access to the secret
 data "aws_iam_policy_document" "example" {
   statement {
-    sid    = "EnableAnotherAWSAccountToReadTheSecret"
+    sid    = "AllowReadSecret"
     effect = "Allow"
 
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::123456789012:root"]
-    }
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
 
-    actions   = ["secretsmanager:GetSecretValue"]
-    resources = ["*"]
+    resources = [
+      aws_secretsmanager_secret.example.arn
+    ]
   }
 }
 
-resource "aws_secretsmanager_secret_policy" "example" {
-  secret_arn = aws_secretsmanager_secret.example.arn
-  policy     = data.aws_iam_policy_document.example.json
+# 3. IAM Policy from the policy document
+resource "aws_iam_policy" "example" {
+  name        = "AllowReadExampleSecret"
+  description = "Allows GetSecretValue on example secret"
+  policy      = data.aws_iam_policy_document.example.json
+}
+
+# 4. Create the IAM Role
+resource "aws_iam_role" "iam_role" {
+  name = "example-iam-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com" # or whatever AWS service/principal you want
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# 5. Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "secret_access" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = aws_iam_policy.example.arn
 }
