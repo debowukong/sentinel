d80455135aab25121a111632f169b6371d967366
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-04 09:16:50 +0530

    Latest IAM policy thing for both resource groups and secrets manager

--- main.tf
+++ main.tf
@@ -48,3 +48,53 @@ resource "aws_iam_role_policy_attachment" "secret_access" {
   role       = aws_iam_role.iam_role.name
   policy_arn = aws_iam_policy.example.arn
 }
+
+resource "aws_iam_group_policy" "my_developer_policy" {
+  name  = "my_developer_policy"
+  group = aws_iam_group.my_developers.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_group" "my_developers" {
+  name = "developers"
+  path = "/users/"
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "test"
+  user = aws_iam_user.lb.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+}
