5368b2207315ce8fa8122a5ded13fd17a28aaff8
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-07 17:47:59 +0530

    gfgfhghgfhgngn

--- main.tf
+++ main.tf
@@ -8,26 +8,58 @@ resource "aws_kms_key" "example" {
   description             = "An example symmetric encryption KMS key"
   enable_key_rotation     = true
   deletion_window_in_days = 20
-  tags = {
-    Name        = "example-kms-key"
-    Environment = "dev"
-  }
-}
-
-resource "aws_kms_key" "primary" {
-  region = "us-east-1"
-
-  description             = "Multi-Region primary key"
-  deletion_window_in_days = 30
-  multi_region            = true
-}
-
-resource "aws_kms_replica_key" "replica" {
-  description             = "Multi-Region replica key"
-  deletion_window_in_days = 7
-  primary_key_arn         = aws_kms_key.primary.arn
-  tags = {
-    Name        = "replica-kms-key"
-    Environment = "dev"
-  }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "key-default-1"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow administration of the key"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Alice"
+        },
+        Action = [
+          "kms:ReplicateKey",
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow use of the key"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Bob"
+        },
+        Action = [
+          "kms:DescribeKey",
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey",
+          "kms:GenerateDataKeyWithoutPlaintext"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
