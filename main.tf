e3bce2030f78718a9098b6ef48098b83fa6b6a17
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-09 13:17:01 +0530

    fdfdfdfdfd

--- main.tf
+++ main.tf
@@ -2,25 +2,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_kms_key" "example1" {
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "example" {
   description             = "An example symmetric encryption KMS key"
   enable_key_rotation     = true
   deletion_window_in_days = 20
+  rotation_period_in_days = 30
   policy = jsonencode({
     Version = "2012-10-17"
     Id      = "key-default-1"
     Statement = [
       {
         Sid    = "Enable IAM User Permissions"
-        Effect = "Allow",
-        Principal = "*",
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
         Action   = "kms:*"
         Resource = "*"
       },
       {
         Sid    = "Allow administration of the key"
-        Effect = "Allow",
-        Principal = "*",
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Alice"
+        },
         Action = [
           "kms:ReplicateKey",
           "kms:Create*",
@@ -42,7 +49,7 @@ resource "aws_kms_key" "example1" {
         Sid    = "Allow use of the key"
         Effect = "Allow"
         Principal = {
-          AWS = "*"
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Bob"
         },
         Action = [
           "kms:DescribeKey",
@@ -52,31 +59,6 @@ resource "aws_kms_key" "example1" {
           "kms:GenerateDataKey",
           "kms:GenerateDataKeyWithoutPlaintext"
         ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_kms_key" "example2" {
-  description             = "An example symmetric encryption KMS key"
-  enable_key_rotation     = true
-  deletion_window_in_days = 20
-}
-
-resource "aws_kms_key_policy" "example2" {
-  key_id = aws_kms_key.example2.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "key-default-1"
-    Statement = [
-      {
-        Sid    = "Enable IAM User Permissions"
-        Effect = "Allow"
-        Principal = {
-          AWS = "*"
-        },
-        Action   = "kms:*"
         Resource = "*"
       }
     ]
