45bd091f385b0edd93c6892a1a06c652a811daab
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-10 12:17:49 +0530

    feat:added

--- main.tf
+++ main.tf
@@ -2,65 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_caller_identity" "current" {}
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
 
-resource "aws_kms_key" "example" {
-  description             = "An example symmetric encryption KMS key"
-  enable_key_rotation     = true
-  deletion_window_in_days = 20
-  rotation_period_in_days = 365
-  policy = jsonencode({
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Id      = "key-default-1"
     Statement = [
       {
-        Sid    = "Enable IAM User Permissions"
+        Action = "sts:AssumeRole"
         Effect = "Allow"
+        Sid    = ""
         Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action   = "kms:*"
-        Resource = "*"
+          Service = "ec2.amazonaws.com"
+        }
       },
-      {
-        Sid    = "Allow administration of the key"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Alice"
-        },
-        Action = [
-          "kms:ReplicateKey",
-          "kms:Create*",
-          "kms:Describe*",
-          "kms:Enable*",
-          "kms:List*",
-          "kms:Put*",
-          "kms:Update*",
-          "kms:Revoke*",
-          "kms:Disable*",
-          "kms:Get*",
-          "kms:Delete*",
-          "kms:ScheduleKeyDeletion",
-          "kms:CancelKeyDeletion"
-        ],
-        Resource = "*"
-      },
-      {
-        Sid    = "Allow use of the key"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Bob"
-        },
-        Action = [
-          "kms:DescribeKey",
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey",
-          "kms:GenerateDataKeyWithoutPlaintext"
-        ],
-        Resource = "*"
-      }
     ]
   })
+
+  max_session_duration = 3600
+
+  tags = {
+    tag-key = "tag-value"
+  }
 }
