c1532da21838293c2886712c58da241b4c85d628
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-11 18:21:26 +0530

    dgf

--- main.tf
+++ main.tf
@@ -1,32 +1,6 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_iam_policy" "policy" {
-  name        = "test_policy"
-  path        = "/"
-  description = "My test policy"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-        Condition = {
-          IpAddress = {
-            "aws:SourceIp" = [
-              "203.0.113.0/24",
-              "198.51.100.0/24"
-            ]
-          }
-        }
-      },
-    ]
-  })
+resource "aws_ebs_snapshot_block_public_access" "example" {
+  state = "block-all-sharing"
 }
