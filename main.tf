a5c02c9d202d59cdf9f29a0885b9654c790086cf
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-17 13:56:53 +0530

    hehtrt

--- main.tf
+++ main.tf
@@ -3,8 +3,64 @@ resource "aws_cloudwatch_log_group" "example" {
   retention_in_days = 14
 }
 
+# IAM role for AWS Glue job
+resource "aws_iam_role" "glue_service_role" {
+  name = "AWSGlueServiceRole-example"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach AWS managed policy for Glue
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = aws_iam_role.glue_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+# Additional policy for S3 access
+resource "aws_iam_role_policy" "glue_s3_policy" {
+  name = "GlueS3Policy"
+  role = aws_iam_role.glue_service_role.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::your-bucket-name/*",
+          "arn:aws:s3:::your-bucket-name"
+        ]
+      }
+    ]
+  })
+}
+
 resource "aws_glue_job" "example" {
-  # ... other configuration ...
+  name     = "example-glue-job"
+  role_arn = aws_iam_role.glue_service_role.arn
+  
+  command {
+    name            = "glueetl"
+    script_location = "s3://your-bucket-name/scripts/your-glue-script.py"
+    python_version  = "3"
+  }
 
   default_arguments = {
     # CloudWatch logging configuration
