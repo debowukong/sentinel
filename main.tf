318ce6b931406c397c5f8ec6da01f7d61d2c6796
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-07-28 11:15:31 +0530

    LADSFDFD

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   backend "s3" {
     bucket = "122610525295-bucket-state-file-karpenter"
-    region = "ap-southeast-2"
+    region = "us-east-1"
     key    = "karpenter.tfstate"
   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.61.0"
+      version = "5.79.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
