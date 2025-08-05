e1f24be9e16f34f9ebfa125580f11d165a98871f
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-08-05 17:37:37 +0530

    FAIL TEST CASE ADDED

--- main.tf
+++ main.tf
@@ -13,17 +13,4 @@ resource "aws_efs_file_system" "foo" {
 resource "aws_efs_access_point" "test" {
   file_system_id = aws_efs_file_system.foo.id
   
-  posix_user {
-    uid = 1000
-    gid = 1000
-  }
-  
-  root_directory {
-    path = "/data"
-    creation_info {
-      owner_uid   = 1000
-      owner_gid   = 1000
-      permissions = "755"
-    }
-  }
 }
