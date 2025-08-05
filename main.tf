a7588484effa89b93343dc515a2f586eb45a5ed4
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-08-05 12:21:00 +0530

    EFS Access Points

--- main.tf
+++ main.tf
@@ -1,47 +1,17 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_backup_vault" "example" {
-  name = "example"
-}
-
-data "aws_iam_policy_document" "example" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_caller_identity.current.account_id]
-    }
-
-    actions = [
-      "backup:DescribeBackupVault",
-      "backup:DeleteBackupVault",
-      "backup:PutBackupVaultAccessPolicy",
-      "backup:DeleteBackupVaultAccessPolicy",
-      "backup:GetBackupVaultAccessPolicy",
-      "backup:StartBackupJob",
-      "backup:GetBackupVaultNotifications",
-      "backup:PutBackupVaultNotifications",
-    ]
-
-    resources = [aws_backup_vault.example.arn]
-
-    # Adding conditions to enforce security best practices
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["true"] # Ensure requests are made over HTTPS
-    }
-
-    condition {
-      test     = "StringEqualsIfExists"
-      variable = "aws:RequestTag/Environment"
-      values   = ["Production"] # Ensure actions are tagged with 'Production' environment
+resource "aws_efs_access_point" "test" {
+  file_system_id = aws_efs_file_system.foo.id
+  
+  posix_user {
+    uid = 1000
+    gid = 1000
+  }
+  
+  root_directory {
+    path = "/data"
+    creation_info {
+      owner_uid   = 1000
+      owner_gid   = 1000
+      permissions = "755"
     }
   }
-}
-
-resource "aws_backup_vault_policy" "example" {
-  backup_vault_name = aws_backup_vault.example.name
-  policy            = data.aws_iam_policy_document.example.json
 }
