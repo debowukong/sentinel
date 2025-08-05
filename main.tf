fb247be0540d7973403ff49daccdaae931f2033f
epicgamesacc643@gmail.com <debojyoti.kar@gds.ey.com>
2025-08-05 12:29:30 +0530

    EFS

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+resource "aws_efs_file_system" "foo" {
+  creation_token = "my-efs-file-system"
+  
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+  
+  tags = {
+    Name = "MyEFSFileSystem"
+  }
+}
+
 resource "aws_efs_access_point" "test" {
   file_system_id = aws_efs_file_system.foo.id
   
